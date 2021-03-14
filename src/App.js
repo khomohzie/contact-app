550a79e638c0268479c48a8dc2d38fc15d117dc7
Daniel Komolafe <komozy2000@gmail.com>
2021-03-14 16:05:16 +0100

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
+const App = () => {
+
+  const [contacts, setContacts] = useState([]);
+
+  useEffect( () => {
+
+    fetch("https://randomuser.me/api/?results=4")
+      .then( function(response) {
+        return response.json();
+      } )
+      .then( (data) => {
+        console.log(data);
+        setContacts(data.results);
+      } )
+
+  }, []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      { contacts.map( contact => (
+        <ContactCard 
+          avatar={contact.picture.large}
+          name={contact.name.first + " " + contact.name.last}
+          email={contact.email}
+          key={contact.email}
+          age={contact.dob.age}
+        />
+      ) ) }
+    </>
+
   );
+
+}
+
+const ContactCard = (props) => {
+
+  const [showAge, setShowAge] = useState(false);
+
+  return (
+
+    <div className="contact-card">
+      <img src={props.avatar} alt="profile" />
+      <div className="user-details">
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        { showAge === true ? <p>Age: {props.age}</p> : null }
+        <button onClick={ () => setShowAge(!showAge) }>
+          Toggle
+        </button>
+      </div>
+    </div>
+
+    // { showAge && <p>Age: 25</p> }
+
+  );
+
 }
 
 export default App;
